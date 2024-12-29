36f239c19dec54089da8e9d143ee0d5f07c10db8
GitHub Action <action@github.com>
2024-12-29 09:48:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/nmotod/ideadrupal8namespacedetector2/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/nmotod/ideadrupal8namespacedetector2/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.nmotod.ideadrupal8namespacedetector2.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nmotod.ideadrupal8namespacedetector2.MyBundle
+import com.github.nmotod.ideadrupal8namespacedetector2.services.MyProjectService
 import javax.swing.JButton
 
 
