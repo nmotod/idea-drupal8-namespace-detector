b75984abf30e73f91e6c264869154302d21aa195
nmotod <toobu9b+code@gmail.com>
2025-06-07 19:12:29 +0900

    feat: add null checks

--- src/main/kotlin/org/masamotod/idea/Drupal8NamespaceDetector/DetectAction.kt
+++ src/main/kotlin/org/masamotod/idea/Drupal8NamespaceDetector/DetectAction.kt
@@ -2,6 +2,7 @@ package org.masamotod.idea.Drupal8NamespaceDetector
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.module.PrimaryModuleManager
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.vfs.LocalFileSystem
@@ -9,23 +10,33 @@ import com.jetbrains.php.drupal.settings.DrupalDataService
 
 class DetectAction : AnAction() {
 
+    private val logger = logger<DetectAction>()
+
     override fun actionPerformed(action: AnActionEvent) {
-        val project = action.project ?: return
+        val project = action.project
+        if (project == null) {
+            logger.warn("Action performed without a project context.")
+            return
+        }
 
         val drupalDataService = DrupalDataService.getInstance(project)
-
         if (!drupalDataService.isConfigValid) {
             Notifier.notifyDrupalSupportRequired(project)
             return
         }
 
         val drupalRoot = LocalFileSystem.getInstance().findFileByPath(drupalDataService.drupalPath)
+        if (drupalRoot == null) {
+            logger.warn("Drupal root (${drupalDataService.drupalPath}) not found for project: ${project.name}")
+            return
+        }
 
-        val primaryModule = PrimaryModuleManager.findPrimaryModule(project) ?: return
+        val primaryModule = PrimaryModuleManager.findPrimaryModule(project)
+        if (primaryModule == null) {
+            logger.warn("Primary module not found for project: ${project.name}")
+            return
+        }
 
-        ProgressManager.getInstance().run(DetectorTask(
-            primaryModule,
-            drupalRoot ?: return
-        ))
+        ProgressManager.getInstance().run(DetectorTask(primaryModule, drupalRoot))
     }
 }
